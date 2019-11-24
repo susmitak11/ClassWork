fcb8f35c0f59ef67f2c1ebdf7943380bed1a095a
Susmita <mishrasusmita560@gmail.com>
2019-11-25 12:31:52 +1300

    Add project files.

---
+++ ClassWorkDatabase/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ClassWorkDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ViewBtn = new System.Windows.Forms.Button();
+            this.BtnCount = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(203, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(530, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ViewBtn
+            // 
+            this.ViewBtn.Location = new System.Drawing.Point(71, 80);
+            this.ViewBtn.Name = "ViewBtn";
+            this.ViewBtn.Size = new System.Drawing.Size(75, 81);
+            this.ViewBtn.TabIndex = 1;
+            this.ViewBtn.Text = "ViewBtn";
+            this.ViewBtn.UseVisualStyleBackColor = true;
+            this.ViewBtn.Click += new System.EventHandler(this.ViewBtn_Click_1);
+            // 
+            // BtnCount
+            // 
+            this.BtnCount.Location = new System.Drawing.Point(372, 266);
+            this.BtnCount.Name = "BtnCount";
+            this.BtnCount.Size = new System.Drawing.Size(114, 67);
+            this.BtnCount.TabIndex = 2;
+            this.BtnCount.Text = "BtnCount";
+            this.BtnCount.UseVisualStyleBackColor = true;
+            this.BtnCount.Click += new System.EventHandler(this.BtnCount_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnCount);
+            this.Controls.Add(this.ViewBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ViewBtn;
+        private System.Windows.Forms.Button BtnCount;
+    }
+}
+
